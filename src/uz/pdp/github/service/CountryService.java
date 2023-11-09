c7db16286d58ac039be86659eb136fed6737efac
Mehrojbek <mehrojbek970510@gmail.com>
2023-11-09 20:14:30 +0500

    second commit

---
+++ src/uz/pdp/github/service/CountryService.java
@@ -0,0 +1,9 @@
+package uz.pdp.github.service;
+
+public class CountryService {
+
+    public static void main(String[] args) {
+        System.out.println("Men country service da ishlayapman");
+    }
+
+}
---
+++ src/uz/pdp/github/service/PersonService.java
@@ -0,0 +1,9 @@
+package uz.pdp.github.service;
+
+public class PersonService {
+
+    public static void main(String[] args) {
+        System.out.println("Men person service da ishlayapman");
+    }
+
+}
